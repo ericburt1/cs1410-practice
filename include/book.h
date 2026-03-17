3553aa08c6d8ad14274984e6275ff19932ef6921
ericburt1 <112107869+ericburt1@users.noreply.github.com>
2026-03-17 02:14:05 +0000

    Finished Activity 8

---
+++ include/book.h
@@ -0,0 +1,27 @@
+#ifndef __BOOK_H__
+#define __BOOK_H__
+
+#include <iostream>
+#include <string>
+
+class Book {
+private:
+    std::string title;
+    int year;
+    Author author;
+
+public:
+    Book(const std::string& t, int y, const Author& a);
+
+    std::string getTitle() const;
+    int getYear() const;
+    Author getAuthor() const;
+
+    void setTitle(const std::string& a);
+    void setYear(int y);
+    void setAuthor(const Author& a);
+
+    void show() const;
+};
+
+#endif
---
+++ src/book.cpp
@@ -0,0 +1,17 @@
+#include "author.h"
+#include "book.h"
+
+Book::Book(const std::string& t, int y, const Author& a) : title(t), year(y), author(a){}
+
+std::string Book::getTitle() const {return title;}
+int Book::getYear() const {return year;}
+Author Book::getAuthor() const {return author;}
+
+void Book::setTitle(const std::string& a){title = a;}
+void Book::setYear(int y){year = y;}
+void Book::setAuthor(const Author& a){author = a;}
+
+void Book::show() const{
+    std::cout << "Book: " << title << " ( " << year << " )\n"
+        << "    Author: " << author.getName() << "[" << author.getNationality() << "]\n";
+}
